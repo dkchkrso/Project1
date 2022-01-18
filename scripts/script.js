6738b77c0fe2198d6162c00da02de82c68859d1f
Christian Kroer Sørensen <dkchkrso@lego.com>
2022-01-18 16:50:46 +0100

    added Intro, level mechanics, bug fixes

--- scripts/script.js
+++ scripts/script.js
@@ -5,21 +5,68 @@ const ctx = canvas.getContext("2d");
 canvas.width = 600;
 canvas.height = 700;
 
-const background = new Image();  // width, height
-background.src = "img/backgrounds/test2.png";
+const bgCity = new Image();  // width, height
+bgCity.src = "img/backgrounds/city.png";
+const bgHighway = new Image();  // width, height
+bgHighway.src = "img/backgrounds/highway.png";
+const bgForest = new Image();  // width, height
+bgForest.src = "img/backgrounds/forest.jpg";
 
 const keys = [];
 const shots = [];
 const enemies = [];
-// const gameLevels = {
-//     gameLevel: 1,
+const imgs = [];
+
+//!new Audio('audio/music/the_end_piano.mp3').play()
+//!
+let audio = new Audio();
+audio.src = 'audio/music/the_end_piano.mp3';
+audio.play;
+// Sound
+// Documentation:
+//!
+//!
 
 
-// }
+// Sound takes three arguments. The source url of the sound, the volume (from 0 to 100), and the loop (true to loop, false not to loop).
+// stop allow to start after (contrary to remove).
+// init re-set the argument volume and loop.
 
-// let allowedEnemiesOnScreen = 2;
-// let enemyCount = 0;
-// let levelEnemiesTotal = 3;
+// Example:
+
+// var foo = new Sound("url", 100, true);
+// foo.start();
+// foo.stop();
+// foo.start();
+// foo.init(100, false);
+// foo.remove();
+
+
+
+//imgs.push("img/backgrounds/virus1.jpeg");
+imgs.push("img/backgrounds/virus2.jpg");
+imgs.push("img/backgrounds/virus3.webp");
+imgs.push("img/backgrounds/virus4.jpg");
+imgs.push("img/backgrounds/virus5.webp");
+imgs.push("img/backgrounds/virus6.jpg");
+imgs.push("img/backgrounds/virus7.jpg");
+imgs.push("img/backgrounds/virus8.png");
+
+const gameTitle = "Covid19 - The Last Hope";
+let str1 = gameTitle
+let str2 = "The year is 2072 and the covid19 virus has been" 
+let str3 = "causing havac on the Earth for more than 5 decades."
+let str4 = "Throughout the year the virus has mutated countless of times"
+let str5 = "and more than 99% of earth population is currently infected with the virus…"
+let str6 = "Only one city remains uninfected with the virus - The Last Hope."
+let str7 = "You are one of the citizens in The Last Hope and you have been tasked"
+let str8 = "to guard the northern gates for the day."
+let str9 = "Little do you know that a swarm of infected people" 
+let str10 = "are on their way to The Last City."
+let str11 = "If the gates are breached all hope for humanity is lost."
+let str12 = "Be prepared for the longest day of your life"
+
+
 
 const hero = new Character(200, 0, 5);
 
@@ -45,7 +92,7 @@ function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH){
 }
 
 class Enemy {
-    constructor(_x, _y, _speed, _enemyInterval, _enemySelect){
+    constructor(_x, _y, _speed, _health, _enemyInterval, _enemySelect){
     this.width = 64; //  832 / 13 = 
     this.height = 64; //  1344 /21
     this.x = Math.random() * (canvas.width - 64);
@@ -57,21 +104,22 @@ class Enemy {
     this.enemyTimer = 0;
     this.enemySelect = 0; //default enemy selection
     this.radius = 20;
-    this.health = 10;
+    this.health = _health;
     //this.direction = Math.random - 0,5 // between 0,5 and -0,5
     }
     move() {
-        this.y++;
+        this.y = this.y + this.speed;
     }
     show(){
-        
         ctx.beginPath();
         ctx.arc(this.x , this.y, this.radius, 0, 2 * Math.PI, false); //int x, int y, int width, int height, int startAngle, int arcAngle
         ctx.fillStyle = "rgb(0, 255, 0)";
         ctx.fill();
 
-        // ctx.fillStyle = "rgb(0, 255, 0)"
-        // ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.fillStyle = "rgb(255, 0, 0)";
+        ctx.font = "22px Comic Sans MS";
+        ctx.textAlign = "center";
+        ctx.fillText(this.health, this.x, this.y - 25);
     }
 }
 
@@ -106,6 +154,7 @@ class Shot {
         ctx.arc(this.x , this.y, this.radius, 0, 2 * Math.PI, false); //int x, int y, int width, int height, int startAngle, int arcAngle
         ctx.fillStyle = "rgb(255, 0, 0)";
         ctx.fill();
+
     }
     move(){
         this.y -= this.speed
@@ -122,7 +171,7 @@ class Shot {
 //smash * 1 [21]
 
 function actionHero(){
-    if (keys[38] && hero.y > 400 || keys[87] && hero.y > 400){ //UP(38) W(87)
+    if (keys[38] && hero.y > 250 || keys[87] && hero.y > 250){ //UP(38) W(87)
         hero.y -= hero.speed;
         hero.frameY = 8;
         hero.moving = true;
@@ -145,6 +194,7 @@ function actionHero(){
         hero.moving = true;
 
     }
+
     // if (keys[32]){  // SPACEBAR(32)
     //     //Add new shot to array
     //     let shotCount = shots.length
@@ -183,7 +233,7 @@ document.addEventListener('keyup', (e) => {
     }
 });
 
-console.log(keyStatus)
+// console.log(keyStatus)
 
   function handlePlayerFrame(){
     if (hero.frameX < 8 && hero.moving === true) {
@@ -193,92 +243,254 @@ console.log(keyStatus)
     }
 }
 
-let allowedEnemiesOnScreen = 2;
-let enemyCount = 0;
-let levelEnemiesTotal = 3;
-    
+let gameStats = {
+    // cityHealth: 3,
+     //allowedEnemiesOnScreen: 10,
+    //enemyCount: 0,
+    // levelEnemiesTotal: 5
+};
+let gameLevelCurrent = 0;
+
+// let gameStats = [];
+// class gameStat {
+//     constructor(_levelNumber, _cityHealth, _levelEnemiesTotal, _enemySpeed){
+//         this.levelNumber = _levelNumber, // intro --> controls
+//         this.cityHealth = _cityHealth,
+//         this.allowedEnemiesOnScreen = 1000,
+//         this.enemyCount = 0,
+//         this.levelEnemiesTotal = _levelEnemiesTotal,
+//         this.enemySpeed = _enemySpeed,
+//         this.levelCompleted = false
+//     }
+// }
+
+let gameLevelStats = [
+    {
+    levelNumber: 0, // intro --> controls
+    cityHealth: 5,
+    allowedEnemiesOnScreen: 1000,
+    enemyCount: 0,
+    levelEnemiesTotal: 3,
+    enemySpeed: 0.5,
+    enemyHealth: 3,
+    levelCompleted: false
+    },
+    {
+     levelNumber: 1,
+     cityHealth: 5,
+     allowedEnemiesOnScreen: 1000,
+     enemyCount: 0,
+     levelEnemiesTotal: 10,
+     enemySpeed: 1,
+     enemyHealth: 3,
+     levelCompleted: false
+    },
+    {
+     levelNumber: 2,
+     cityHealth: 5,
+     allowedEnemiesOnScreen: 1000,
+     enemyCount: 0,
+     levelEnemiesTotal: 20,
+     enemySpeed: 1.5,
+     enemyHealth: 3,
+     levelCompleted: false
+    },
+    {
+    levelNumber: 3,
+    cityHealth: 5,
+    allowedEnemiesOnScreen: 1000,
+    enemyCount: 0,
+    levelEnemiesTotal: 20,
+    enemySpeed: 1.5,
+    enemyHealth: 3,
+    levelCompleted: false
+    },
+    {
+    levelNumber: 4,
+    cityHealth: 5,
+    allowedEnemiesOnScreen: 1000,
+    enemyCount: 0,
+    levelEnemiesTotal: 20,
+    enemySpeed: 1.5,
+    levelCompleted: false
+    }
+
+];
+
+
+
+
+
+
+// Covid19 – The Last Hope
+// The year is 2072 and the covid19 virus has been causing havac on the Earth for more than 5 decades.
+// Throughout the year the virus has mutated countless of times and more than 99% of earth population is currently infected with the virus… 
+// Only one city remains uninfected with the virus - “The Last Hope”.
+// You are one of the citizens in The Last Hope and you have been tasked to guard the northern gates for the day. Little do you know that a swarm of infected people are on their way to The Last City. If the gates are breached all hope for humanity is lost.
+// Be prepared for the longest day of your life
+// User the arrows or (a, s, d, w) to move player
+// Press spacebar to shoot
+// City Healds is displayed in the bottom of the screen 
+
+
+let gameStarted = false;
+
 let lastTime = 1;
 function animate(timeStamp){
     ctx.clearRect(0,0,canvas.width, canvas.height)
     const deltaTime = timeStamp - lastTime;
     lastTime = timeStamp;
-    //console.log(deltaTime);
 
-    ctx.fillStyle = 'grey';
-    ctx.fillRect(0, canvas.height-100, scoreBoard.width, scoreBoard.height)
+    //Intro
+    if (gameStarted === false){
+        //Background rotation - every 10 sec.
+        const img = document.getElementById("imgVirus")
+        img.setAttribute("src", imgs[Math.floor(timeStamp / 10000 % imgs.length)]) //Math.floor(Math.random() * imgs.length)
+        
 
-    ctx.drawImage(background, 0, 400, canvas.width , 200)
-    drawSprite(playerSprite, hero.width * hero.frameX, hero.height * hero.frameY, hero.width, hero.height, hero.x, hero.y, hero.width, hero.height)
-    actionHero();
+        //Text
+        ctx.fillStyle = "rgb(255, 255, 255)";
+        ctx.font = "35px Comic Sans MS";
+        ctx.textAlign = "center";
+        ctx.fillText(str1, 300, 150);
+        ctx.font = "16px Comic Sans MS";
+        ctx.fillText(str2, 300, 220);
+        ctx.fillText(str3, 300, 250);
+        ctx.fillText(str4, 300, 310);
+        ctx.fillText(str5, 300, 340);
+        ctx.fillText(str6, 300, 370);
+        ctx.fillText(str7, 300, 430);
+        ctx.fillText(str8, 300, 460);
+        ctx.fillText(str9, 300, 490);
+        ctx.fillText(str10, 300, 520);
+        ctx.fillText(str11, 300, 550);
+        ctx.fillText(str12, 300, 610);
 
-    //Shots
-    for (let i = 0; i < shots.length; i++){
-        shots[i].show();
-        shots[i].move();
-        //Check out of canvas
-        if (shots[i].y < 3){ //array clean up
-            shots.splice(i, 1);
-            break;
-        }
-        //Check collision
-        if (enemies.length !== 0){
-        for (let j = 0; j < enemies.length; j++){
-            let distance = Math.sqrt(Math.pow((shots[i].x - enemies[j].x),2) + Math.pow((shots[i].y - enemies[j].y),2));
-            if (distance <= (shots[i].radius + enemies[j].radius)){
-                //ENEMY HIT
+        //Mouse Click
+        canvas.addEventListener("click", () => {
+            gameStarted = true;
+        });
+        //Enter Key
+        document.addEventListener('keydown', (e) => {
+            if (e.keyCode === 13){
+                gameStarted = true;
+            }
+            });
+
+    } else {    
+    
+        //Background and Scoreboard
+        ctx.fillStyle = 'grey';
+        ctx.fillRect(0, canvas.height-100, scoreBoard.width, scoreBoard.height)
+
+        ctx.drawImage(bgHighway, 0, 300, canvas.width , 300)
+        ctx.drawImage(bgCity, 0, 400, canvas.width , 200)
+        ctx.drawImage(bgForest, 0, 0, canvas.width , 300)
+        
+        //Hero
+            drawSprite(playerSprite, hero.width * hero.frameX, hero.height * hero.frameY, hero.width, hero.height, hero.x, hero.y, hero.width, hero.height)
+            actionHero();
+ 
+        //Shots
+        for (let i = 0; i < shots.length; i++){
+            shots[i].show();
+            shots[i].move();
+            //Check out of canvas
+            if (shots[i].y < 3){ //array clean up
                 shots.splice(i, 1);
-                enemies[j].health--
-                    if(enemies[j].health <= 0){
-                        enemies.splice(j, 1);
-                    }
                 break;
             }
+            //Check collision
+            if (enemies.length !== 0){
+            for (let j = 0; j < enemies.length; j++){
+                let distance = Math.sqrt(Math.pow((shots[i].x - enemies[j].x),2) + Math.pow((shots[i].y - enemies[j].y),2));
+                if (distance <= (shots[i].radius + enemies[j].radius)){
+                    //ENEMY HIT
+                    shots.splice(i, 1);
+                    enemies[j].health--
+                        if(enemies[j].health <= 0){
+                            enemies.splice(j, 1);
+                        }
+                    break;
+                }
+            }
+            } 
         }
-        } 
-    }
 
-    //Enemies
-    if (!enemies.length && enemyCount < levelEnemiesTotal){
-        const enemy = new Enemy (200, 100, 50, 50, Math.floor((Math.random() * (characterSprites.length))))
-        enemies.push(enemy);
-        enemyCount++  
-    }
-    for (let i = 0; i < enemies.length; i++){
-        //drawSprite(playerSprite, hero.width * hero.frameX, hero.height * hero.frameY, hero.width, hero.height, hero.x, hero.y, hero.width, hero.height)
-        enemies[i].show();
-        enemies[i].move();
-
-        if (i === enemies.length - 1 && 
-                  enemies.length < allowedEnemiesOnScreen && 
-                  enemies[i].enemyInterval < enemies[i].enemyTimer &&
-                  enemyCount <= levelEnemiesTotal){
-            const enemy = new Enemy(200, 100, 50, 50, Math.floor((Math.random() * (characterSprites.length))))
+        //Enemies
+        if (!enemies.length && 
+            gameLevelStats[gameLevelCurrent].enemyCount < gameLevelStats[gameLevelCurrent].levelEnemiesTotal && 
+            gameLevelStats[gameLevelCurrent].levelCompleted === false){
+            const enemy = new Enemy (200, 100, gameLevelStats[gameLevelCurrent].enemySpeed, gameLevelStats[gameLevelCurrent].enemyHealth, 50, Math.floor((Math.random() * (characterSprites.length))))
             enemies.push(enemy);
-            enemyCount++
-        } else {
-            enemies[i].enemyTimer++
+            gameLevelStats[gameLevelCurrent].enemyCount++  
+        }
+        for (let i = 0; i < enemies.length; i++){
+            //drawSprite(playerSprite, hero.width * hero.frameX, hero.height * hero.frameY, hero.width, hero.height, hero.x, hero.y, hero.width, hero.height)
+            enemies[i].show();
+            enemies[i].move();
+
+            if (i === enemies.length - 1 && 
+                    enemies.length < gameLevelStats[gameLevelCurrent].allowedEnemiesOnScreen && 
+                    enemies[i].enemyInterval < enemies[i].enemyTimer &&
+                    gameLevelStats[gameLevelCurrent].enemyCount < gameLevelStats[gameLevelCurrent].levelEnemiesTotal){
+                const enemy = new Enemy(200, 100, gameLevelStats[gameLevelCurrent].enemySpeed, gameLevelStats[gameLevelCurrent].enemyHealth, 50, Math.floor((Math.random() * (characterSprites.length))))
+                enemies.push(enemy);
+                gameLevelStats[gameLevelCurrent].enemyCount++
+            } else {
+                enemies[i].enemyTimer++
+            }
+
+            if (enemies[i].y > 550){
+                enemies.splice(i, 1);
+                gameLevelStats[gameLevelCurrent].cityHealth--;
+            }
         }
 
-        if (enemies[i].y > 550){
-            enemies.splice(i, 1);
+        //Check Win
+        if (gameLevelStats[gameLevelCurrent].enemyCount >= gameLevelStats[gameLevelCurrent].levelEnemiesTotal && //Number of enemies been present
+            enemies.length === 0 && //enemies array is empty
+            gameLevelStats[gameLevelCurrent].cityHealth > 0){ //City is still alive
+            
+            gameLevelStats[gameLevelCurrent].levelCompleted = true;
+            console.log("gameLevel" + gameLevelCurrent);
+        
+        } else if (gameLevelStats[gameLevelCurrent].cityHealth <= 0){
+            ctx.fillStyle = "rgb(255, 0, 0)";
+            ctx.font = "35px Comic Sans MS strong";
+            ctx.textAlign = "center";
+            //ctx.rotate(20 * Math.PI / 180);
+            ctx.fillText("The city has been infected with Corona", 300, 400);
+            ctx.fillText("You Lose", 300, 350);
+            
+            // const youLose = new Image();
+            // youLose.src = "img/Utility/youLose.png";
+            //ctx.drawImage(youLose, 0, 100, canvas.width , 400)
         }
-    }
+        ctx.fillText("City Health: " + gameLevelStats[gameLevelCurrent].cityHealth, 100, 620);
 
-    //Check Win/Lose
-    if (enemyCount >= levelEnemiesTotal && enemies.length === 0){
-        const youLose = new Image();
-        youLose.src = "img/Utility/youLose.png";
-        ctx.drawImage(youLose, 0, 100, canvas.width , 400)
-    }
+        if (gameLevelStats[gameLevelCurrent].levelCompleted === true){
+            
+            ctx.fillStyle = "rgb(255, 255, 255)";
+            ctx.font = "25px Comic Sans MS";
+            ctx.textAlign = "center";
+            ctx.fillText("Level Cleared", 300, 400);
+            ctx.font = "18px Comic Sans MS";
+            ctx.textAlign = "center";
+            ctx.fillText(`Press ENTER to start level ${gameLevelStats[gameLevelCurrent].levelNumber + 1} `, 300, 450);
+        }
 
-    // console.log("allowedEnemiesOnScreen " + allowedEnemiesOnScreen);
-    // console.log("enemyCount " + enemyCount);
-    // console.log("levelEnemiesTotal " + levelEnemiesTotal);
-    // console.log("enemies " + enemies.length);
-    // console.log("shots " + shots.length); 
-
-    handlePlayerFrame();
-    requestAnimationFrame(animate);
+            document.addEventListener('keydown', (e) => {
+            if (e.keyCode === 13){
+                if ( gameLevelStats[gameLevelCurrent].levelCompleted === true){
+                         gameLevelCurrent++;
+                }
+            }
+            });
+        
+    };
+handlePlayerFrame();
+requestAnimationFrame(animate);
 };
 animate(0);
 
